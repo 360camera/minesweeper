7b39466daf44074da0c8adb4e4537d0e8b33349b
360camera <52750551+360camera@users.noreply.github.com>
2022-09-27 16:06:33 -0400

    Add loss state
    Added game over, cleaned up commented out code

--- Minesweeper.py
+++ Minesweeper.py
@@ -7,8 +7,6 @@ class Minesweeper:
     # Use self.board to see the complete board
     # Use self.obscuredBoard to see a solvable board
 
-    # Internal functions
-
     def placeMine(self, board, placeX, placeY): # Place a mine on the board
         board[placeY][placeX] = "X"
         # Increment tiles around mine
@@ -46,37 +44,6 @@ class Minesweeper:
                     elif coords != (0,0) and actualTile != "X":
                         self.obscuredBoard[checkY][checkX] = actualTile
 
-    # revealEmpties OLD VERSION (kept in case i mega screwed up)
-
-    """def revealEmpties(self, spaceX, spaceY): # Reveal empty tiles around a space
-        for coords in [(0,0), (1,0), (-1,0), (0,1), (0,-1)]: # in plus shape around tile
-            if spaceY+coords[1] in range(len(self.board)) and spaceX+coords[0] in range(len(self.board[0])):
-                if self.obscuredBoard[spaceY+coords[1]][spaceX+coords[0]] != "?":
-                    print("Already cleared")
-                elif self.board[spaceY+coords[1]][spaceX+coords[0]] == 0:
-                    checkX = spaceX+coords[0]
-                    checkY = spaceY+coords[1]
-                    # Count surrounding mines
-                    mines = 0
-                    for modX in range(-1, 2):
-                        for modY in range(-1, 2):
-                            if modX+checkX in range(len(self.board[0])) and modY+checkY in range(len(self.board)):
-                                if self.board[checkY+modY][checkX+modX] == "X" and (modX, modY) != (0,0):
-                                    mines += 1
-                    # Reveal
-                    self.obscuredBoard[checkY][checkX] = mines
-                    # Repeat if nessecary
-                    #if not isFirst and mines != 0:
-                        #return
-                    if mines == 0: # this may be redundant, need to test
-                        self.revealEmpties(checkX, checkY)
-                    else:
-                        print("No mines")
-                        exit()
-                elif coords != (0,0) and self.board[spaceY+coords[1]][spaceX+coords[0]] != "X":
-                    self.obscuredBoard[spaceY+coords[1]][spaceX+coords[0]] = self.board[spaceY+coords[1]][spaceX+coords[0]]
-                    print("copied")"""
-
     def __init__(self, width, height, mines): # Create a widthXheight board with mines
         # Generate boards
         board = []
@@ -96,14 +63,17 @@ class Minesweeper:
                 minesPlaced += 1
         self.board = board
         self.obscuredBoard = obscuredBoard
+        self.gameState = True
 
     def revealSpace(self, spaceX, spaceY): # Reveal space (spaceX,spaceY) on obscuredBoard
         if self.board[spaceY][spaceX] != 0:
             self.obscuredBoard[spaceY][spaceX] = self.board[spaceY][spaceX]
+            if self.board[spaceY][spaceX] == "X":
+                self.gameState = False # You died
         else:
             self.revealEmpties(spaceX, spaceY)
     
-    def printColor(self, obscure): # Print board with color!
+    def printColor(self, obscure=True): # Print board with color, specify False for completed board
         if obscure:
             usingBoard = self.obscuredBoard
         else:
@@ -124,19 +94,14 @@ class Minesweeper:
             print(line)
         self.printBlack()
 
-
-
 # execution
-t = Minesweeper(10,10,10)
+m = Minesweeper(10,10,10)
 while True:
     os.system("cls")
-    t.printColor(True)
+    m.printColor()
+    if m.gameState == False:
+        print("[red]GAME OVER![/red]")
+        exit()
     x = int(input("X: "))
     y = int(input("Y: "))
-    t.revealSpace(x-1, y-1)
-
-"""for x in t.board:
-    line = ""
-    for y in x:
-        line += str(y) + " "
-    print(line)"""
+    m.revealSpace(x-1, y-1)
